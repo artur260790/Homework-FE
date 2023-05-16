08e6e2c3d1e27c8a453ddfa6fc621f2d3d73e2ab
artur260790 <126571357+artur260790@users.noreply.github.com>
2023-05-16 18:31:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-// const num = Number(prompt('Введите число'));
-// const result = num * 0.1;
-// console.log(`10% от числа ${num} равно ${result}`);
+// const numbers = document.createElement('div');
+// numbers.classList.add('numbers');
+// document.body.prepend(numbers);
 
-// const num1 = Number(prompt('Введите первое число'));
-// const num2 = Number(prompt('Введите второе число'));
-// const minNum = Math.min(num1, num2);
-// console.log(`Наименьшее число: ${minNum}`);
-
-// const num = Number(prompt('Введите число'));
-// if (num < 100) {
-//   console.log('Число меньше 100');
-// } else if (num > 100) {
-//   console.log('Число больше 100');
-// } else {
-//   console.log('Число равно 100');
+// for (let i = 100; i >= 50; i -= 10){
+// const para = document.createElement('p');
+// para.innerText = i;
+// numbers.append(para);
 // }
 
-// const name = prompt('Введите ваше имя');
-// const age = Number(prompt('Введите ваш возраст'));
-// if (age >= 18) {
-//   console.log(`Hello, ${name}`);
-// } else {
-//   console.log(`Hi, ${name}`);
+// const strings_container = document.createElement('div');
+//  strings_container.classList.add('numbers');
+//  document.body.prepend(string_container);
+
+//  const strings = ['Artur', 'Maxim', 'Sergej','Anton'];
+//  for(let i=0; i< array.length; i++){
+//     const para = document.createElement('p');
+//     para.innerText = string[i];
+//     strings_container.append(para);
+
+//  }
+// const users = [
+//     { first_name: 'Ivan', last_name: 'Ivanov', age: 15 },
+//     { first_name: 'Petr', last_name: 'Petrov', age: 37 },
+//     { first_name: 'Anna', last_name: 'Ivanova', age: 56 },
+//   ];
+ 
+//  const users_container = document.createElement('div');
+//   users_container.classList.add('usersContainer');
+//   document.body.prepend(users_container);
+
+// for (let i = 0; i < users.length; i++) {
+//     const userCard = document.createElement('div');
+//     const userFirstName = document.createElement('p');
+//     const userLastName = document.createElement('p');
+//     const userAge = document.createElement('p');
+
+//     userCard.append(userFirstName, userLastName, userAge);
+//     users_container.append(userCard);
+
+// userFirstName.innerText = users[i].first_name;
+// userLastName.innerText = users[i].last_name;
+// userAge.innerText = users[i].age;
+
+
 // }
